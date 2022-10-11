3204fd94c0c630564f45f55afa75bbb351953d06
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-11 18:20:15 +0800

    connect to mongodb w/ mongoose

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const dotenv = require('dotenv');
+
+const { connectMDB } = require('./db');
+
+// env, configs
+dotenv.config({
+  path: './config/config.env'
+});
+
+// connect
+connectMDB().catch(error =>
+  console.error('connect-mongodb Error', error.stack)
+);
 
 const app = express();
 
 const PORT = Number(process.env.PORT) || 3000;
+
+// import routes
+const { userRouter } = require('./routes/user.route');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/dist', express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.json('root');
-});
+app.use('/', [userRouter]);
 
 app.post('/signin', (req, res) => {
   const { email, password } = req.body;
@@ -39,8 +54,16 @@ app.post('/profile', (req, res) => {
   res.json(user);
 });
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}/`);
+//
+mongoose.connection.once('open', () => {
+  app.listen(PORT, err => {
+    console.log(`Face-Detect Server is running on http://localhost:${PORT}`);
+  });
+});
+
+// listen to mongoose error
+mongoose.connection.on('error', err => {
+  console.error('error @mongoose-conn-error ---', err);
 });
 
 /**
