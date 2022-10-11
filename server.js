e34386dd5f36da8e2f8e5d2243b457ed3d544863
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-11 13:51:24 +0800

    add index.html, make profile, signin, register routes

--- server.js
+++ server.js
@@ -1,18 +1,51 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const PORT = Number(process.env.PORT) || 3000;
 
-// app.use(express.bodyParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/dist', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-	
-	res.json('root')
-})
+  res.json('root');
+});
 
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
 
+  res.json('signin');
+});
 
+app.post('/register', (req, res) => {
+  res.json('register');
+});
 
-app.listen(3000, ()=>{
-	console.log(`http://localhost:3000/`)
-})
+app.get('/profile/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  res.json(userId);
+});
+
+app.post('/profile', (req, res) => {
+  const user = {
+    name: 'Sally',
+    hobby: 'soccer'
+  };
+
+  res.json(user);
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}/`);
+});
+
+/**
+ * /signin --> POST = success/fail
+ * /register --> POST = user
+ * /profile/:userId --> GET = user
+ * /image --> PUT --> user
+ */
